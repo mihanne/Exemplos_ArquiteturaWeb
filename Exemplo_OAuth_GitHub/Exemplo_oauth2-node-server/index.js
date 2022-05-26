bffe762fac1e065b24423d71c08fed7dfb31b3c9
Michelle Hanne Soares de Andrade <michelle.hanne@ormuztech.com.br>
2022-05-26 19:00:20 -0300

    Add files via upload

---
+++ Exemplo_OAuth_GitHub/Exemplo_oauth2-node-server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+var cors = require("cors");
+
+const CLIENT_ID = "cdc923255bd5d3e884a0";
+const CLIENT_SECRET = "de846b4a6d0e302e1e40c69f3570553e36f11232";
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
+
+const app = express();
+app.use(cors({ credentials: true, origin: true }));
+
+app.get("/oauth/redirect", (req, res) => {
+  axios({
+    method: "POST",
+    url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+    headers: {
+      Accept: "application/json",
+    },
+  }).then((response) => {
+    res.redirect(
+      `http://localhost:3000?access_token=${response.data.access_token}`
+    );
+  });
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
