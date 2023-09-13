963a7b08ddf8ac514680376ac14e35aa0ffd6783
Michelle Hanne Soares de Andrade <michellehanne.andrade@gmail.com>
2023-09-13 18:14:07 -0300

    ExemploCRUDMysql

---
+++ Exemplo_CRUD2_MySQL/db.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+
+// create the connection to database
+export const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+
+  connection.connect(
+    function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+    }
+});
